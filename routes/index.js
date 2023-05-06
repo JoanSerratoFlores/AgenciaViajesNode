e3a3a66d8ae1545c8f7e99bf6ec9506d89d15be0
JoanSerratoFlores <joan.serrato.flores@gmail.com>
2023-05-06 13:52:23 -0600

    Proyecto Agencia de Viajes Parte 03

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,33 @@ import express from "express";
 
 const router = express.Router();
 
-router.get('/', (req,res) => { //req- lo que enviamos  --- res - lo que express nos responde
-    
-    res.render('Inicio');
+router.get("/", (req, res) => {
+  //req- lo que enviamos  --- res - lo que express nos responde
 
-}); 
+  res.render("Inicio", {
+    pagina: "Inicio",
+  });
+});
 
-router.get('/Nosotros', (req,res) => { //req- lo que enviamos  --- res - lo que express nos responde
-    
-    const viajes = 'Viajes a Alemania';
-    
-    res.render('Nosotros', {
-    
-        viajes
-    
+router.get("/Nosotros", (req, res) => {
+  //req- lo que enviamos  --- res - lo que express nos responde
+  res.render("Nosotros", {
+    pagina: "Nosotros",
+  });
+});
+
+router.get("/Viajes", (req, res) => {
+  //req- lo que enviamos  --- res - lo que express nos responde
+  res.render("Viajes", { 
+    pagina: "Viajes", 
+  });
+});
+
+router.get("/Testimoniales", (req, res) => {
+    //req- lo que enviamos  --- res - lo que express nos responde
+    res.render("Testimoniales", { 
+      pagina: "Testimoniales", 
     });
+  });
 
-}); 
-
-router.get('/Contacto', (req,res) => { //req- lo que enviamos  --- res - lo que express nos responde
-    res.send('Contacto');
-}); 
-
-export default router
+export default router;
